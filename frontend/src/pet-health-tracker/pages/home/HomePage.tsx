f185933bca35b1069e8ab77692784f4ab7879955
Retford <retfordjb@gmail.com>
2025-12-06 15:16:11 -0500

    refactor(homepage): endpoint vaccinat events added

--- frontend/src/pet-health-tracker/pages/home/HomePage.tsx
+++ frontend/src/pet-health-tracker/pages/home/HomePage.tsx
@@ -5,17 +5,21 @@ import { CardReminders } from './components/cards/reminder/CardReminders';
 import { CardServices } from './components/cards/service/CardServices';
 import { useGetPets } from './hooks/useGetPets';
 import { PetSkeleton } from './components/skeletons/PetSkeleton';
+import { useGetVaccinateEvents } from './hooks/useGetVaccinateEvents';
 
 export const HomePage = () => {
   const user = useAuthStore((state) => state.user);
 
   const { data: pets = [], isLoading } = useGetPets();
+  const petsId = pets.map((pet) => pet.id);
+
+  const { data: vaccinateEvents = [] } = useGetVaccinateEvents(petsId);
 
   return (
     <div className='gap-4 flex flex-col'>
       <CardProfile name={user?.fullName || ''} />
       {isLoading ? <PetSkeleton /> : <CardPets pets={pets} />}
-      <CardReminders />
+      <CardReminders vaccinateEvents={vaccinateEvents} />
       <CardServices />
     </div>
   );
